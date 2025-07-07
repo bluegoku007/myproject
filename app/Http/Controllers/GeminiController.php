3d1c790941678494fe56243f72dfdd91f861a866
bluegoku007 <selmenselmi5@gmail.com>
2025-07-07 12:07:11 +0100

    7/7

--- app/Http/Controllers/GeminiController.php
+++ app/Http/Controllers/GeminiController.php
@@ -78,7 +78,7 @@ public function getActivities(Request $request)
         'max_tokens' => 1000,
         'temperature' => 0.7,
     ]);
-#ssss
+#sssssssss
     if ($response->successful()) {
         $data = $response->json();
         $answer = $data['choices'][0]['message']['content'] ?? 'No response from AI';
